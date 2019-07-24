03717d26d53509bda81352c7b4deb837ce649aa3
霍然 <303176152@qq.com>
2019-07-24 16:57:38 +0800

    添加全局请求

---
+++ src/utils/axios.js
@@ -0,0 +1,34 @@
+const VueAxios = {
+  vm: {},
+  // eslint-disable-next-line no-unused-vars
+  install(Vue, instance) {
+    if (this.installed) {
+      return;
+    }
+    this.installed = true;
+
+    if (!instance) {
+      // eslint-disable-next-line no-console
+      console.error("You have to install axios");
+      return;
+    }
+
+    Vue.axios = instance;
+
+    // 通过 Object.defineProperties 为 VNode 的原型绑定了对象 axios,$http
+    Object.defineProperties(Vue.prototype, {
+      axios: {
+        get: function get() {
+          return instance;
+        }
+      },
+      $http: {
+        get: function get() {
+          return instance;
+        }
+      }
+    });
+  }
+};
+
+export { VueAxios };
