26dd4ea0f501ad881e904b73a7ea01db5dc9fc4c
霍然 <303176152@qq.com>
2019-07-24 16:37:10 +0800

    搭建请求框架

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vuex from "vuex";
+
+import user from "./modules/user";
+
+export default new Vuex.Store({
+  modules: {
+    user
+  },
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {}
+});
--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="main">
     <el-form :model="validateForm" ref="validateForm" class="user-layout-login">
-      <el-tabs style="textAlign:center;borderBottom:unset">
-        <el-tab-pane label="账号密码登录">
+      <el-tabs
+        v-model="activeName"
+        style="textAlign:center;borderBottom:unset"
+        @tab-click="handleClick"
+      >
+        <el-tab-pane label="账号密码登录" name="account">
           <el-form-item prop="username" :rules="[
           {required: true, message: '请输入账号'}]">
             <el-input placeholder="账号：admin" v-model="validateForm.username">
               <i slot="prefix" class="el-icon-user"></i>
             </el-input>
           </el-form-item>
-
           <el-form-item prop="password" :rules="[
           {required: true, message: '请输入密码'}]">
             <el-input placeholder="密码：admin" v-model="validateForm.password">
@@ -18,7 +21,26 @@
           </el-form-item>
         </el-tab-pane>
 
-        <el-tab-pane label="手机号登录">手机号登录</el-tab-pane>
+        <el-tab-pane label="手机号登录" name="mobile">
+          <el-form-item prop="phone" :rules="[{required: true, message: '请输入手机号'}]">
+            <el-input placeholder="手机号" v-model="validateForm.phone">
+              <i slot="prefix" class="el-icon-mobile-phone"></i>
+            </el-input>
+          </el-form-item>
+
+          <el-row :gutter="16">
+            <el-col :span="16">
+              <el-form-item prop="captcha" :rules="[{required: true, message: '请输入验证码'}]">
+                <el-input placeholder="验证码" v-model="validateForm.captcha">
+                  <i slot="prefix" class="el-icon-message"></i>
+                </el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-button>获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
       </el-tabs>
 
       <el-form-item>
@@ -40,20 +62,32 @@
 export default {
   data() {
     return {
+      activeName: "account",
       validateForm: {
         username: "",
-        password: ""
+        password: "",
+        phone: "",
+        captcha: ""
       }
     };
   },
   methods: {
+    handleClick(tab) {
+      this.activeName = tab.name;
+    },
+
     submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
+      const { activeName } = this;
+      const validateFieldsKey =
+        activeName === "account"
+          ? ["username", "password"]
+          : ["phone", "captcha"];
+      // 对部分表单进行验证
+      this.$refs.validateForm.validateField(validateFieldsKey, error => {
+        if (error) {
           console.log("error submit!!");
-          return false;
+        } else {
+          console.log("submit!!");
         }
       });
     }
