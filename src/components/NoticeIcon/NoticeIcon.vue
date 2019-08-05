3cdb5249c881e27e06690efce8d36f15e842ceec
霍然 <303176152@qq.com>
2019-08-05 11:22:25 +0800

    添加消息按钮

--- src/components/NoticeIcon/NoticeIcon.vue
+++ src/components/NoticeIcon/NoticeIcon.vue
@@ -1,6 +1,12 @@
 <template>
-  <el-popover>
-    <span class="header-notice">
+  <el-popover
+    placement="bottom"
+    title="标题"
+    width="200"
+    trigger="click"
+    content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+  >
+    <span class="header-notice" slot="reference">
       <el-badge value="12">
         <i style="font-size: 16px; padding: 4px" class="el-icon-bell"></i>
       </el-badge>
@@ -16,6 +22,7 @@ export default {
 .header-notice {
   display: inline-block;
   transition: all 0.3s;
+  height: 100%;
 
   span {
     vertical-align: initial;
